e766d9b31bf79b100a303e9cfc9e9657f9ee8643
Valentin Silvestre <vasilvestre@laposte.net>
2016-12-12 09:08:29 +0100

    Template integration

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,30 +17,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findBy([], [], 8);
         // replace this example code with whatever you need
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->render('AppBundle:Default:index.html.twig', [
+            'products' => $products
+        ]);
     }
 
     public function shopAction(Request $request)
     {
-        $form = $this->get('form.factory')->create(ProductFilterType::class);
-
-        if ($request->query->has($form->getName())) {
-            $form->submit($request->query->get($form->getName()));
-
-            $filterBuilder = $this->get('doctrine.orm.entity_manager')
-                ->getRepository('AppBundle:Product')
-                ->createQueryBuilder('e');
-
-            $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder);
-
-            var_dump($filterBuilder->getDQL());
-        }
-
         $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
 
         return $this->render('@App/Shop/index.html.twig', [
-            'form' => $form->createView(),
             'products' => $products
         ]);
     }
