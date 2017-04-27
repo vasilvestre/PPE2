eda0afdc51475bef0d3dbe767e5c4bd27238c1bb
Valentin Silvestre <vasilvestre@laposte.net>
2017-04-27 16:31:38 +0200

    display promotion

--- src/AppBundle/Controller/PromotionController.php
+++ src/AppBundle/Controller/PromotionController.php
@@ -10,11 +10,12 @@ class PromotionController extends Controller
         $promotions = $this->getDoctrine()->getRepository('AppBundle:Promotion')->findall();
         $productsPromo1 = $this->getDoctrine()->getRepository('AppBundle:Product')->findByPromotion('1');
         $productsPromo2= $this->getDoctrine()->getRepository('AppBundle:Product')->findByPromotion('2');
-        dump($productsPromo2);
-        dump($productsPromo1);
-        return $this->render('@App/Shop/promotions.html.twig',[
-            'productsPromo1' => $productsPromo1,
-            'productPromo2' => $productsPromo2
+
+        $products = [];
+        array_push($products, $productsPromo2);
+        array_push($products, $productsPromo1);
+        return $this->render('@App/Shop/index.html.twig',[
+            'products' => $products[0],
         ]);
     }
 }
